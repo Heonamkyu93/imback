f6048456d75a06949db796ff85473b997de2372a
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-01-15 19:16:42 +0900

    처음 세팅

---
+++ src/main/java/com/refore/our/OurApplication.java
@@ -0,0 +1,13 @@
+package com.refore.our;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/refore/our/OurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refore.our;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
