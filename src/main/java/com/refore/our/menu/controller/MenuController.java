9a9bcebb59b0ba4b12dfa0dcb67c1af530bf1be1
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-05-08 21:31:57 +0900

    menu 추가

--- src/main/java/com/refore/our/menu/controller/MenuController.java
+++ src/main/java/com/refore/our/menu/controller/MenuController.java
@@ -1,19 +1,32 @@
 package com.refore.our.menu.controller;
 
+import com.refore.our.menu.dto.MenuDto;
+import com.refore.our.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MenuController {
 
+    private final MenuService menuService;
+
+
+    @PostMapping()
+    public ResponseEntity<String> menuInsert(@RequestBody MenuDto menuDto){
+        menuService.menuInsert(menuDto);
+
+    }
+
+
 
     @GetMapping()
     public ResponseEntity<String> menuIndex(){
-
+        menuService.menuIndex();
         return ResponseEntity.ok("테스트");
     }
 
